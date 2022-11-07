439b7d58bfd5521ae11bdee9d803d01cfa14a5c2
Sebastián Contreras <85262987+sebac820@users.noreply.github.com>
2022-11-07 03:51:18 -0300

    actualizar metricas

--- funciones_auxiliares.py
+++ funciones_auxiliares.py
@@ -28,6 +28,14 @@ def average_precision(top_n_verdadero: list, recomendaciones: list, arroba: int)
     # other-evaluation-metrics-6881e0ee21a9
 
 
+def configurar_pyplot(xlabel='', ylabel=''):
+    plt.figure(figsize=(16, 9))
+    plt.xlabel(xlabel, fontsize='x-large')
+    plt.xticks(fontsize='large')
+    plt.ylabel(ylabel, fontsize='x-large')
+    plt.yticks(fontsize='large')
+
+
 def dcg_usuario(top_n_verdadero: list, recomendaciones: list, arroba: int):
     # Evitar errores de "index out of range":
     limite_top_n_verdadero = min(arroba, len(top_n_verdadero))
@@ -48,10 +56,6 @@ def dcg_usuario(top_n_verdadero: list, recomendaciones: list, arroba: int):
     return dcg
 
 
-def distancia(item_1, item_2):
-    return scipy.spatial.distance.cosine(item_1, item_2)
-
-
 def obtener_access_token_para_la_api_de_spotify(client_id, client_secret):
     client_id_and_client_secret = client_id + ':' + client_secret
     client_id_and_client_secret = str(b64encode(client_id_and_client_secret.encode()))[2:-1]
@@ -64,22 +68,36 @@ def obtener_access_token_para_la_api_de_spotify(client_id, client_secret):
     return json.loads(response.text)['access_token']
 
 
-def probabilidad_de_conocer_dado_item(item, interacciones, usuarios):
-    usuarios_que_conocen_el_item = interacciones[interacciones['track_id_clean'] == item]['session_id'].unique()
-    return len(usuarios_que_conocen_el_item) / len(usuarios)
+def probabilidad_de_conocer_item(item, interacciones):
+    numero_de_usuarios_que_conocen_el_item = len(interacciones[interacciones['track_id'] == item]['session_id'].unique())
+    numero_de_usuarios = len(interacciones['session_id'].unique())
+    return numero_de_usuarios_que_conocen_el_item / numero_de_usuarios
 
 
-def probabilidad_de_item(item, interacciones):
-    return len(interacciones[interacciones['track_id_clean'] == item]) / len(interacciones)
+def probabilidad_de_item_1_dado_item_2(item_1, item_2, interacciones):
+    usuarios_que_han_accedido_a_item_1 = interacciones[interacciones['track_id'] == item_1]['session_id'].unique()
+    usuarios_que_han_accedido_a_item_2 = interacciones[interacciones['track_id'] == item_2]['session_id'].unique()
+    numero_de_usuarios_que_han_accedido_a_ambos_items = 0
+    for usuario in usuarios_que_han_accedido_a_item_1:
+        if usuario in usuarios_que_han_accedido_a_item_2:
+            numero_de_usuarios_que_han_accedido_a_ambos_items += 1
+    return numero_de_usuarios_que_han_accedido_a_ambos_items / len(usuarios_que_han_accedido_a_item_2)
 
 
-def probabilidad_de_item_dada_lista_de_recomendaciones(item, recomendaciones, items, interacciones):
-    return probabilidad_de_item(item, interacciones) * len(items) / len(recomendaciones)
+def probabilidad_de_item_dado_usuario(item, usuario, interacciones):
+    numero_de_interacciones_con_item = len(interacciones[(interacciones['session_id'] == usuario) & (interacciones['track_id'] == item)])
+    numero_de_interacciones = len(interacciones[interacciones['session_id'] == usuario])
+    return numero_de_interacciones_con_item / numero_de_interacciones
 
 
-def configurar_pyplot(xlabel='', ylabel=''):
-    plt.figure(figsize=(16, 9))
-    plt.xlabel(xlabel, fontsize='x-large')
-    plt.xticks(fontsize='large')
-    plt.ylabel(ylabel, fontsize='x-large')
-    plt.yticks(fontsize='large')
+def probabilidad_de_que_item_sea_conocido_por_usuario(item, usuario, interacciones):
+    probabilidades = 0
+    perfil_de_usuario = interacciones[interacciones['session_id'] == usuario]['track_id'].unique()
+    for otro_item in perfil_de_usuario:
+        probabilidades += (
+            probabilidad_de_conocer_item(otro_item, interacciones) *
+            probabilidad_de_item_1_dado_item_2(item, otro_item, interacciones) *
+            probabilidad_de_item_dado_usuario(otro_item, usuario, interacciones) /
+            probabilidad_de_item_dado_usuario(item, usuario, interacciones)
+        )
+    return probabilidades
--- metricas.py
+++ metricas.py
@@ -1,8 +1,8 @@
 from funciones_auxiliares import (
-    average_precision, dcg_usuario, distancia, probabilidad_de_conocer_dado_item,
-    probabilidad_de_item_dada_lista_de_recomendaciones
+    average_precision, dcg_usuario, probabilidad_de_que_item_sea_conocido_por_usuario
 )
 from math import log2
+import scipy.spatial.distance
 
 
 # NDCG
@@ -17,8 +17,7 @@ def ndcg(top_n_verdadero_por_usuario: dict, recomendaciones: dict, arroba: int):
         idcg += 1 / log2(i + 1)
     # Para cada usuario, calcular su NDCG y agregarlo a la suma de NDCGs:
     for usuario, top_n_verdadero in top_n_verdadero_por_usuario.items():
-        recomendaciones_usuario = recomendaciones[usuario]
-        suma_ndcgs += dcg_usuario(top_n_verdadero, recomendaciones_usuario, arroba) / idcg
+        suma_ndcgs += dcg_usuario(top_n_verdadero, recomendaciones[usuario], arroba) / idcg
     # Retornar NDCG promedio:
     return suma_ndcgs / len(top_n_verdadero_por_usuario)
 
@@ -27,35 +26,44 @@ def ndcg(top_n_verdadero_por_usuario: dict, recomendaciones: dict, arroba: int):
 
 
 def mean_average_precision(top_n_verdadero_por_usuario: dict, recomendaciones: dict, arroba: int):
-    # Inicializar suma de average precisions:
     suma_de_average_precisions = 0
-    # Para cada usuario, calcular su average precision y agregarlo a la suma de average precisions:
     for usuario, top_n_verdadero in top_n_verdadero_por_usuario.items():
         suma_de_average_precisions += average_precision(top_n_verdadero, recomendaciones[usuario], arroba)
-    # Retornar average precision promedio:
     return suma_de_average_precisions / len(top_n_verdadero_por_usuario)
 
 
 # Novedad
 
 
-def novedad(recomendaciones, items, interacciones):
-    novedad = 0
+def novelty_for_single_user(recomendaciones, usuario, interacciones):
+    novedades = 0
     for item in recomendaciones:
-        novedad += (
-            probabilidad_de_item_dada_lista_de_recomendaciones(item, recomendaciones, items, interacciones)
-            *
-            (1 - probabilidad_de_conocer_dado_item(item))
-        )
-    return novedad
+        novedades += 1 - probabilidad_de_que_item_sea_conocido_por_usuario(item, usuario, interacciones)
+    return novedades / len(recomendaciones)
+
+
+def novelty_for_multiple_users(recommendations, interactions):
+    novelty = 0
+    for user, recommended_items in recommendations:
+        novelty += novelty_for_single_user(recommended_items, user, interactions)
+    return novelty / len(recommendations)
 
 
 # Diversidad
 
 
-def diversidad(recomendaciones):
+def diversity_for_single_user(recommendations: list, item_features):
     distancias = 0
-    for n in range(1, len(recomendaciones)):
+    for n in range(1, len(recommendations)):
+        item_n = item_features[item_features['track_id'] == recommendations[n]].drop(columns=['track_id'])
         for k in range(0, n):
-            distancias += distancia(recomendaciones[n], recomendaciones[k])
-    return 2 * distancias / (len(recomendaciones) * (len(recomendaciones) - 1))
+            item_k = item_features[item_features['track_id'] == recommendations[k]].drop(columns=['track_id'])
+            distancias += scipy.spatial.distance.cosine(item_n, item_k)
+    return 2 * distancias / (len(recommendations) * (len(recommendations) - 1))
+
+
+def diversity_for_multiple_users(recommendations: dict, item_features):
+    diversity = 0
+    for recommended_items in recommendations.values():
+        diversity += diversity_for_single_user(recommended_items, item_features)
+    return diversity / len(recommendations)
